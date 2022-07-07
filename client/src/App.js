e0a834d3bff5d5cd6d2504cd58889cee41ebcc44
Edmond GAKS <edmondgaks@gmail.com>
2022-07-07 15:10:41 +0200

    rendering the first words on the page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
+import './App.css';
 
+import { ChannelContainer,ChannelListContainer } from './components/index';
 const apiKey  = 'mckkzdgpkx2f';
-
+const client = StreamChat.getInstance(apiKey);
 const App = () => {
   return (
     <div className='app__wrapper'>
-        <Chat></Chat>
+        <Chat client={client} theme="team light">
+            <ChannelListContainer />
+            <ChannelContainer />
+        </Chat>
     </div>
   )
 }
