27e381d35f79ce5de87b18b41cb2a54726730005
Edmond GAKS <edmondgaks@gmail.com>
2022-07-07 15:29:16 +0200

    adding the simple sidebar and the whole assets folder

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { ChannelList,useChatContext } from 'stream-chat-react';
+import { ChannelSearch,TeamChannelList,TeamChannelPreview } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import LogoutIcon from '../assets/logout.png'
+import HospitalIcon from '../assets/hospital.png'
+const Sidebar =() => (
+    <div className='channel-list__sidebar'>
+        <div className='channel-list__sidebar-icon1'>
+            <div className='icon1__inner'>
+                <img src={HospitalIcon} alt='Hospital' width="30" />
+            </div>
+        </div>
+        <div className='channel-list__sidebar-icon2'>
+            <div className='icon1__inner'>
+                <img src={LogoutIcon} alt='Logout' width="30" />
+            </div>
+        </div>
+    </div>
+)
 const ChannelListContainer = () => {
   return (
-    <div>ChannelListContainer</div>
+    <>
+        <Sidebar />
+    </>
   )
 }
 
