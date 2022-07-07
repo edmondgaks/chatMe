e099650fb7d68df599db04a28db18ded58ec4957
Edmond GAKS <edmondgaks@gmail.com>
2022-07-07 14:53:43 +0200

    adding some components that are going to be used

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const apiKey  = 'mckkzdgpkx2f';
 
 const App = () => {
   return (
-    <div>
-        <h1>ChatMe</h1>
+    <div className='app__wrapper'>
+        <Chat></Chat>
     </div>
   )
 }
