8396e0de573e28bd18c211edb8ed477abb79a2db
Edmond GAKS <edmondgaks@gmail.com>
2022-07-09 13:25:12 +0200

    getting user information using cookies

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,27 @@ import Cookies from 'universal-cookie';
 import './App.css';
 
 import { ChannelContainer,ChannelListContainer,Auth } from './components';
+
+const cookies = new Cookies();
+
 const apiKey  = 'mckkzdgpkx2f';
 const client = StreamChat.getInstance(apiKey);
-const authToken = false;
+
+
+const authToken = cookies.get("token");
+if(authToken) {
+  client.connectUser({
+    token: cookies.get('token'),
+    username: cookies.get('username'),
+    fullName: cookies.get('fullName'),
+    userId: cookies.get('userId'),
+    phoneNumber: cookies.get('phoneNumber'),
+    avatarURL: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword'),
+  })
+}
 const App = () => {
-  if (!authToken) return <Auth />
+  if (authToken) return <Auth />
   return (
     <div className='app__wrapper'>
         <Chat client={client} theme="team light">
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,8 +1,9 @@
 import React, { useState} from 'react'
 import Cookies from 'universal-cookie';
-// import axios from 'axios';
+import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg';
+const cookies = new Cookies();
 const initialState = {
     fullName: '',
     username: '',
@@ -17,9 +18,24 @@ const Auth = () => {
     const handleChange = (e) => {
         setForm({...form, [e.target.name]: e.target.value  });
     }
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(form);
+        const {fullName,username,password,phoneNumber,avatarURL} = form;
+
+        const URL = 'http://localhost:5000/auth';
+        const { data: { token,userId,hashedPassword } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`,{
+            fullName,username,password,phoneNumber,avatarURL 
+        });
+        cookies.set('token',token);
+        cookies.set('username',username);
+        cookies.set('fullName',fullName);
+        cookies.set('userId',userId);
+        if(isSignup) {
+            cookies.set('phoneNumber',phoneNumber);
+            cookies.set('avatarURL',avatarURL);
+            cookies.set('hashedPassword',hashedPassword);
+        }
+        window.location.reload();
     }
     const switchMode = () => {
         setisSignup((prevIsSignup) => !prevIsSignup);
