1d959dafab4eb995b639f24e949d68b7eaf235cd
Edmond GAKS <edmondgaks@gmail.com>
2022-07-09 12:39:52 +0200

    adding our secrets and working on the backend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,23 @@
+const crypto = require('crypto');
+const {connect} = require('getstream');
+const bcrypt = require('bcrypt');
+const StreamChat = require('stream-chat');
+
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_API_ID;
+
 const signup = (req,res) => {
+    try {
+        const {fullName,username,password,phoneNumber} = req.body;
+        const userId = crypto.randomBytes(16).toString('hex');
+        const serverClient = connect(api_key,api_secret,app_id);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error});
+    }
+};
+const login = (req,res) => {
     try {
         
     } catch (error) {
@@ -6,7 +25,6 @@ const signup = (req,res) => {
         res.status(500).json({ message: error});
     }
 };
-const login = (req,res) => {};
 
 
 module.exports = {signup,login};
