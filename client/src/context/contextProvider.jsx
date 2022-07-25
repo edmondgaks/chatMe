23868706359d95b63b69c3a31140302891953cb6
Edmond GAKS <edmondgaks@gmail.com>
2022-07-25 23:10:08 +0200

    adding the the usestates to be used

--- client/src/context/contextProvider.jsx
+++ client/src/context/contextProvider.jsx
@@ -2,3 +2,10 @@ import React, { createContext,useContext,useState } from 'react';
 
 const StateContext = createContext();
 
+export const ContextProvider = ({ children }) => {
+    const [activeMenu,setActiveMenu] = useState(true);
+    const [isClicked,setisClicked] = useState(initialState);
+    const [screenSize,setScreenSize] = useState(undefined);
+    
+    
+}
